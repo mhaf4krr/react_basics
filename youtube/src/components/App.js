67256771c1c536a0c241e1ff081ec61d46bbcbb5
mhaf4krr <hyderdevelops@yahoo.com>
2020-07-24 20:52:46 +0530

    Youtube Done

--- youtube/src/components/App.js
+++ youtube/src/components/App.js
@@ -9,6 +9,10 @@ class App extends React.Component {
   // handles the video list
   state = { videos: [] };
 
+  componentDidMount() {
+    this.onTermSearch("Bella Ciao");
+  }
+
   onTermSearch = async (term) => {
     console.log(term);
     let response = await youtube.get("/search", {
@@ -17,7 +21,10 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items, selectedVideo: null });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -29,11 +36,21 @@ class App extends React.Component {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- youtube/src/components/VideoDetail.js
+++ youtube/src/components/VideoDetail.js
@@ -4,7 +4,20 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div> ... Loading ...</div>;
   }
-  return <div> {video.snippet.title}</div>;
+
+  const videoSrc = `https://youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div>
+      <div className='ui embed'>
+        <iframe src={videoSrc} />
+      </div>
+      <div className='ui segment'>
+        <h4 className='ui header'>{video.snippet.title}</h4>
+      </div>
+
+      <p>{video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
